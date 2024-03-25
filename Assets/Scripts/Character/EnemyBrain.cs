e9cb3a51f8b4130819680a376087deb9b10a17ff
jmcbriggs <joshuabriggswork@gmail.com>
2024-03-25 21:52:16 +0000

    Update EnemyBrain.cs

--- Assets/Scripts/Character/EnemyBrain.cs
+++ Assets/Scripts/Character/EnemyBrain.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.U2D.Aseprite;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 [RequireComponent(typeof(EnemyCharacter))]
 public class EnemyBrain : MonoBehaviour
